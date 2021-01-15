ba5024e90db914e418680b958d74fc847ab10d4f
Nick Gray <nickgray87@gmail.com>
2021-01-15 16:03:01 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -3,3 +3,24 @@ colors = ['red', 'blue', 'green', 'purple']
 def using_push(array = colors, string = 'pink')
   array.push(string)
 end
+
+def using_unshift(array = colors, string = 'yellow')
+  array.unshift(string)
+end
+
+def using_pop(array =colors)
+  array.pop 
+end
+
+def pop_with_args(array = colors)
+  array.pop(2)
+end
+
+def using_shift(array = colors)
+  array.shift 
+end
+
+def shift_with_args(array = colors)
+  array.shift(2)
+end
+
